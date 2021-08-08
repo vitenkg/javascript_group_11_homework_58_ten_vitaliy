b9ab146b59cc008cbaf7a75285b3e78e4a0417e5
Vitaliy Ten <vitalten10@gmail.com>
2021-08-08 16:09:08 +0600

    Что-то пошло не так, папка не попала в "GIT" Добавил заново проект и удалил не нужное

---
+++ homework_58/src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import ModalBuilder from "./Containers/ModalBuilder/ModalBuilder";
+
+const App = () => (
+    <ModalBuilder />
+);
+
+export default App;
---
+++ homework_58/src/Containers/ModalBuilder/ModalBuilder.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ModalBuilder = () => {
+
+    return (
+        <div>
+            Something
+        </div>
+    );
+};
+
+export default ModalBuilder;
